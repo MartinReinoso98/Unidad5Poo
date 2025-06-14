43ceb27695298c0b9c0d875b9f4d27744df34af1
jzmttv <jmzv1001@gmail.com>
2025-06-14 11:50:26 -0300

    app.py
    funcionalidad dos??

--- app.py
+++ app.py
@@ -64,12 +64,11 @@ def verificar_entrada():
             #GET - mostrar formulario
     return render_template('formulario.html')
 
-'''
 
-#FUNCIONALIDAD 2
-@app.route('/templates/registro.html', methods=['GET', 'POST'])
-def registrarSalida():
-       if request.method == 'POST':
+#FUNCIONALIDAD 2   
+@app.route('/templates/formulario.html', methods=['GET', 'POST'])
+def verificar_salida():
+    if request.method == 'POST':
         legajo = request.form.get('legajo')
         dni = str(request.form.get('dni_4'))
 
@@ -82,42 +81,48 @@ def registrarSalida():
             trabajador.legajo == legajo,
             trabajador.dni.like(f"%{dni}")
         ).first()
+
         print("trabajador")
         print(trabajador_existente)
-        if trabajador_existente:
-            #verificar
-            fecha_actual = datetime.today().date()
-            print("trabajdor:",trabajador_existente.id)
-            registro_existente = registrohorario.query.filter_by(
+        if trabajador_existente:   #si trabajador es distinto de None
+            
+            fecha_actual = datetime.today().date()  #guarda la fehca de hoy 
+            print("trabajdor:",trabajador_existente.id)  #Imprime en consola el ID del trabajador que se encontró previamente en la base de datos. Verifica que trabajador_existente contiene un objeto válido
+            registro_existente = registrohorario.query.filter_by(  #Busca en la tabla registrohorario registros que cumplan las dos condiciones
                 idtrabajador=trabajador_existente.id,
                 fecha=fecha_actual
-            ).first()
+            ).first() # La funcion first() Retorna el primer registro que cumpla las condiciones o None si no existe
             print(f"hay un registro:{registro_existente}")
 
-            if registro_existente:
-                return render_template('error.html', error="Ya se registró una entrada para hoy")
-            else:
-                # Crear registro de horario asociado al trabajador
+            if registro_existente: # si ya hay un registro de estrada para el trabajador en la fecha actual 
+                 # Crear registro de horario asociado al trabajador
                 hora_actual = datetime.now().time()
-                fecha_actual = datetime.today().date()
 
-                nueva_entrada = registrohorario(
-                    fecha=fecha_actual,
-                    horaentrada=hora_actual,
-                    horasalida=None,
-                    dependencia="DO1",
-                    idtrabajador=trabajador_existente.id
-                )
-                database.session.add(nueva_entrada)
-                database.session.commit()
-                return render_template('anuncio.html', anuncio="Nueva entrada registrada")
+                if registro_existente.horasalida == None:
+                    nueva_entrada = registrohorario(
+                        fecha = fecha_actual,
+                        horaentrada = registro_existente.horaentrada, #No se debe modificar la hora de entrada
+                        horasalida = hora_actual,
+                        dependencia = "DO1", #modifcar debe acceder a los datos de la dependencia
+                        idtrabajador = trabajador_existente.id
+                    )
+                    database.session.add(nueva_entrada)
+                    database.session.commit()
+                    return render_template('anuncio.html', anuncio="Horario de Salida registrada")
+                else:
+                    return render_template('error.html', error="Ya se registró una salida para hoy")
+                
+            else:
+               return render_template('error.html', error="Aun no se ha registrado una entrada para hoy")
 
         else:
             return render_template('error.html', error="Trabajador no registrado en la base de datos")
 
             #GET - mostrar formulario
-        return render_template('formulario.html')
+    return render_template('formulario.html')
 
+
+'''
 
 #FUNCIONALIDAD 3
 @app.route('/templates/consulta.html', methods=['GET'])
