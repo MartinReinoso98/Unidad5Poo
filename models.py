0f84d9b594d27198d8fd12fc45ae7bbe704b40dd
MartinReinoso98 <111535334+MartinReinoso98@users.noreply.github.com>
2025-06-16 18:30:57 -0300

    actu models.py

--- models.py
+++ models.py
@@ -6,13 +6,18 @@ database = SQLAlchemy()
 class trabajador(database.Model):
     __tablename__ = 'trabajador'
     id = database.Column(database.Integer, primary_key=True, nullable=False)
-    nombre = database.Column(database.String(20), nullable=False) # nullable hace que la columna no pueda tener NULL
+    nombre = database.Column(database.String(20), nullable=False)  # <!-- nullable hace que la columna no pueda tener NULL -->
     apellido = database.Column(database.String(20), nullable=False)
     dni = database.Column(database.String(10), nullable=False)
     correo = database.Column(database.String(30), nullable=False)
     legajo = database.Column(database.Integer, nullable=False)
     horas = database.Column(database.Integer, nullable=False)
+    funcion = database.Column(database.String(2), nullable=False)   # <!-- 'AD' para administrativo, 'GE' para gerente, 'TE' para técnico -->
     registrohorario = database.relationship('registrohorario', backref='trabajador', cascade="all, delete-orphan")
+    
+    # <!-- sobrecarga -->
+    def __lt__(self, otro):
+        return (self.apellido) < (otro.apellido)
     
     
 class registrohorario(database.Model):
